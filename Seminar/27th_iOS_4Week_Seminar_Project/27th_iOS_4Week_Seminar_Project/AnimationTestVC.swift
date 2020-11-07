b3134d86672c764f1ef91b3a27cc85ab30838867
최영재 <brandon94_@naver.com>
2020-11-07 20:53:42 +0900

    [UPDATE] 세미나 자료

--- Seminar/27th_iOS_4Week_Seminar_Project/27th_iOS_4Week_Seminar_Project/AnimationTestVC.swift
+++ Seminar/27th_iOS_4Week_Seminar_Project/27th_iOS_4Week_Seminar_Project/AnimationTestVC.swift
@@ -17,21 +17,35 @@ class AnimationTestVC: UIViewController {
     }
     
     @IBAction func touchUpStart(_ sender: Any) {
-        UIView.animate(withDuration: 3.0, animations: {
-            self.ImageView.alpha = 0
-            self.ImageView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
-        }) { (finish) in
-            UIView.animate(withDuration: 1.0, animations: {
-                self.ImageView.alpha = 1
-                self.initPosition()
-            })
+//        UIView.animate(withDuration: 3.0, animations: {
+//            self.ImageView.alpha = 0
+//            self.ImageView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+//        }) { (finish) in
+//            UIView.animate(withDuration: 1.0, animations: {
+//                self.ImageView.alpha = 1
+//                self.initPosition()
+//            })
+//        }
+//
+//
+//    }
+//
+//    func initPosition() {
+//        ImageView.frame = CGRect(x: 87, y: 210, width: 240, height: 209)
+//    }
+        let scale = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        let move = CGAffineTransform(translationX: 200, y: 300)
+        let rotate = CGAffineTransform(rotationAngle: .pi/4)
+        
+        let combine = scale.concatenating(move).concatenating(rotate)
+        UIView.animate(withDuration: 2.0, animations: {
+            self.ImageView.transform = combine
+        }) { finish in
+            UIView.animate(withDuration: 1.0){
+                self.ImageView.transform = .identity}
         }
         
-        
-    }
-
-    func initPosition() {
-        ImageView.frame = CGRect(x: 87, y: 210, width: 240, height: 209)
-    }
     
+    
+}
 }
